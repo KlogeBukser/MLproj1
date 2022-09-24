8f880eac6132fff6e12993905d5787ba42cc357e
torstbje <torstbje@uio.no>
2022-09-24 17:15:29 +0200

    Added posibility of having the polynomial features on the x-axis when plotting beta

--- src/poly_funcs.py
+++ src/poly_funcs.py
@@ -4,9 +4,33 @@ def get_2D_pols(deg):
     # Produces every polynomial of two variables up to a given degree
     # Functions take a tuple as input
     # i = i and j = j are necessary to prevent lambda functions from overwriting themselves
+
     pols = []
     for j in range(deg + 1):
         # Outer loop iterates over polynomial degrees
         # Inner loop iterates over polynomials of the degree in the outer loop
         pols += [(lambda xy, i = i, j = j: xy[0]**(j - i) * xy[1]**i) for i in range(j + 1)]
     return pols
+
+def get_2D_string(deg):
+    # Method for collecting the polynomials as strings
+    str_pols = []
+    for j in range(deg + 1):
+        str_pols += [get_poly_text(i,j) for i in range(j + 1)]
+    return str_pols
+
+def get_poly_text(i,j):
+    # Helper function for getting the string version of a specific polynomial
+    # i is the degree of y, while j is the total polynomial degree
+    if (i == j == 0):
+        return "$1$"
+    poly = "$"
+    if (j-i > 0):
+        poly += "x"
+    if (j-i > 1):
+        poly += "^" + str(j-i)
+    if (i > 0):
+        poly += "y"
+    if (i > 1):
+        poly += "^" + str(i)
+    return poly + "$"
--- src/produce_results.py
+++ src/produce_results.py
@@ -5,12 +5,13 @@ from transform import *
 
 import numpy as np
 
-def prod_score_plots(feature_values, label_values, max_poly = 5, include_MSE = True, include_R2 = True, include_beta = True):
-    # (x,y) = features
-    # z = label
+def prod_score_plots(feature_values, label_values, max_poly = 5, include_MSE = True, include_R2 = True, include_beta = True, features_beta = True):
+    # (x,y) = input
+    # z = output
     # Produces plot(s) for measuring quality of 2D polynomial model
     # Plots Mean squared error, R2-score, and beta values
     # Each plot can be disabled by optional boolean parameters
+    # The Beta plot uses features on the x-axis, but numbers can be used instead if (features_beta = False)
 
     poly_degs = np.arange(1,max_poly + 1)
 
@@ -27,8 +28,13 @@ def prod_score_plots(feature_values, label_values, max_poly = 5, include_MSE = T
         R2_vals[i] = R2(z_train,z_tilde)
         MSE_vals[i] = MSE(z_train,z_tilde)
         if (include_beta):
-            points = np.arange(1,len(beta) + 1, 1)
-            plt.plot(points,beta, label = "Polynomials up to degree %d" % deg)
+            if (features_beta):
+                # Collects the polynomial as a string to use as the x-axis
+                x_ax = get_2D_string(deg)
+            else:
+                # Uses integers on the x-axis
+                x_ax = np.arange(0,len(beta),1)
+            plt.plot(x_ax,beta, label = "Polynomials up to degree %d" % deg)
 
     if (include_beta):
         title_axlabs("beta values when adding more features","Features","beta")
