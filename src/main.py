3800de22297f9e54e7d1a633a7e5a2fa5455422c
torstbje <torstbje@uio.no>
2022-09-24 13:57:02 +0200

    Made function for producing plots of error scores for 2D polynomials of different sizes

--- src/main.py
+++ src/main.py
@@ -6,20 +6,9 @@ from generate import *
 from calculate import *
 from poly_funcs import *
 from transform import *
+from plotting import *
+from produce_results import *
 
 x,z = generate_data_Franke()
 
-poly_deg = np.arange(1,6,1)
-R2_vals = np.zeros(5)
-
-for deg in poly_deg:
-
-    funcs = get_2D_pols(deg)
-    X = calc_design(x,funcs)
-    X_train, X_test, z_train, z_test = split_scale(X,z)
-    z_tilde = get_model(X_train,z_train)
-    R2_vals[deg-1] = R2(z_train,z_tilde)
-
-
-plt.plot(poly_deg,R2_vals)
-plt.show()
+prod_score_plots(x, z, max_poly = 5, include_R2 = True)
---
+++ src/produce_results.py
@@ -0,0 +1,32 @@
+from plotting import *
+from calculate import *
+from poly_funcs import *
+from transform import *
+
+import numpy as np
+
+def prod_score_plots(feature_values, label_values, max_poly = 5, include_MSE = True, include_R2 = False):
+    # (x,y) = features
+    # z = label
+    # Produces plot(s) for measuring quality of model
+    # Plots Mean squared error by default, but R2 can be plotted as well/instead (in separate plot)
+
+    poly_degs = np.arange(1,max_poly + 1)
+
+    MSE_vals = np.zeros(max_poly)
+    R2_vals = np.zeros(max_poly)
+
+    for i,deg in enumerate(poly_degs):
+
+        funcs = get_2D_pols(deg)
+        X = calc_design(feature_values,funcs)
+        X_train, X_test, z_train, z_test = split_scale(feature_values,label_values)
+        z_tilde = get_model(X_train,z_train)
+        R2_vals[i] = R2(z_train,z_tilde)
+        MSE_vals[i] = MSE(z_train,z_tilde)
+
+    if (include_R2):
+        simple_plot(poly_degs,R2_vals, "R$^2$")
+
+    if (include_MSE):
+        simple_plot(poly_degs,MSE_vals,"Mean Squared Error")
