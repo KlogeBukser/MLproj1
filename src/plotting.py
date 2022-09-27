954b18faf587b5470ce81deddbfff9dae623fdea
KlogeBukser <imvacant0923@gmail.com>
2022-09-27 15:22:09 +0200

    added useful plotting functions for 2d and surface plots

--- src/plotting.py
+++ src/plotting.py
@@ -1,4 +1,16 @@
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+import os
+
+
+ERR_NOT_ENUF_COLORS = 'Colors should be the same length as or longer plot_count'
+ERR_TOO_MANY_PLOTS = 'Too many plots on a single figure'
+ERR_NOT_ENUF_LABELS = 'Label count should match plot count'
+COLORS = 'brgcmykw' # all default colours
+
 
 def simple_plot(x,y,title = "Temporary title",xlab = "x",ylab = "y"):
     # Simple plot of 2 vectors
@@ -29,3 +41,66 @@ def title_axlabs(title,xlab,ylab):
     plt.title(title)
     plt.xlabel(xlab)
     plt.ylabel(ylab)
+
+def set_paras(x_title,y_title,title,filename,file_dir):
+
+    '''set all the parameters in the figure and save files'''
+    plt.legend()
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.title(title)
+
+    if filename:
+        full_path = os.path.join(file_dir, filename)
+        plt.savefig(full_path)
+        plt.close()
+    else:
+        plt.show()
+
+
+def is_valid(plot_count,color, label):
+    '''check input validity'''
+    assert plot_count <= 8, ERR_TOO_MANY_PLOTS
+    if color:
+        assert len(color) >= plot_count, ERR_NOT_ENUF_COLORS
+    if label:
+        assert len(label) == plot_count, ERR_TOO_MANY_PLOTS
+
+def plot_2D(x, y, plot_count=1,title=None,x_title=None,y_title=None,label=None,filename=None,
+        file_dir='',color=COLORS):
+
+    '''plots inputs: x:array like of array like, y:array like of array likes, 
+    plot_count:int(number of plots),title:string, file_dir:string,colour:string'''
+
+    is_valid(plot_count, color, label)
+
+    make_dir(file_dir)
+
+    for i in range(plot_count):
+        if label:
+            plt.plot(x[i],y[i],label=label[i],color=color[i])
+        else:
+            plt.plot(x[i],y[i],label=label,color=color[i])
+
+    
+    set_paras(x_title, y_title, title, filename, file_dir)
+
+
+def plot_surface(x,y,z,title=None,x_title=None,y_title=None,z_title=None,label=None,filename=None,
+        file_dir=''):
+
+    # some of the code from this function is given in the project 
+
+    make_dir(file_dir)
+    
+    fig = plt.figure()
+    ax = plt.axes(projection ='3d')
+    
+    surf = ax.plot_surface(x, y, z,cmap ='viridis',
+                       linewidth=0, antialiased=False)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    set_paras(x_title, y_title, title, filename, file_dir)
